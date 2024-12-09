a8ef7dbd386966e0188e2e4c1827fca7715cb7f5
covelitein <abrahamcovenant2004@gmail.com>
2024-12-09 18:33:17 +0100

    project update

--- src/sections/landingPage/Review.tsx
+++ src/sections/landingPage/Review.tsx
@@ -6,7 +6,7 @@ export const Review = () => (
     <div className="max-w-6xl mx-auto md:text-center">
       <h2 className="text-3xl md:text-5xl font-bold md:mb-6">What Our Clients Say</h2>
       <p className="text-gray-600 mb-10">
-        Hear from the people we've helped achieve their goals.
+        Hear from the people we&apos;ve helped achieve their goals.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {reviews.map((review, index) => (
